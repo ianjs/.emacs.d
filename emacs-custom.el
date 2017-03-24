40cda18286dd1ed77c288d2a357b6a0092dd9e45
Ian Slinger <ian@slingers.org>
2017-03-24 16:05:36 +1100

    Tidy up

---
+++ emacs-custom.el
@@ -0,0 +1,28 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default bold shadow italic underline bold bold-italic bold])
+ '(ansi-color-names-vector
+   (vector "#eaeaea" "#d54e53" "#b9ca4a" "#e7c547" "#7aa6da" "#c397d8" "#70c0b1" "#000000"))
+ '(coffee-tab-width 2)
+ '(custom-safe-themes
+   (quote
+    ("52588047a0fe3727e3cd8a90e76d7f078c9bd62c0b246324e557dfa5112e0d0c" "cf08ae4c26cacce2eebff39d129ea0a21c9d7bf70ea9b945588c1c66392578d1" "1157a4055504672be1df1232bed784ba575c60ab44d8e6c7b3800ae76b42f8bd" "9e54a6ac0051987b4296e9276eecc5dfb67fdcd620191ee553f40a9b6d943e78" "5ee12d8250b0952deefc88814cf0672327d7ee70b16344372db9460e9a0e3ffc" default)))
+ '(fci-rule-color "#2a2a2a")
+ '(global-rainbow-delimiters-mode t)
+ '(package-selected-packages
+   (quote
+    (0blayout which-key tagedit swiper smex rainbow-delimiters paredit magit iedit ido-ubiquitous hydra company clojure-mode-extra-font-locking cider aggressive-indent ace-window)))
+ '(send-mail-function nil))
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-level-6 ((t (:foreground "#7fafaf"))))
+ '(org-level-7 ((t (:foreground "dark gray"))))
+ '(org-level-8 ((t (:foreground "#cc9073")))))
