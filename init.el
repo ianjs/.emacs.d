1b4de47a70152b52c0936917ce129fc31fd7e376
Ian Slinger <ian@slingers.org>
2017-08-10 22:10:15 +1000

    half way through merge into config.org

--- init.el
+++ init.el
@@ -1,82 +1,132 @@
-;; init.el
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;  Cloned from here:
+;;    https://github.com/larstvei/dot-emacs
 
-;; Place downloaded elisp files in ~/.emacs.d/vendor. You'll then be able
-;; to "load" them.
-;;
-(add-to-list 'load-path "~/.emacs.d/vendor")
-(add-to-list 'exec-path "/usr/local/bin")
-(add-to-list 'exec-path "~/bin")
-
-;; Add a directory to our load path so that we can `load` them
-(add-to-list 'load-path "~/.emacs.d/customizations")
-
-;;;;;;;;;;;;;;;;; Load all packages in one place
-(load "mypackages.el")
-
-;; Sets up exec-path-from-shell so that Emacs will use the correct
-;; environment variables
-;; Sets up exec-path-from shell
-;; https://github.com/purcell/exec-path-from-shell
-(when (memq window-system '(mac ns))
-  (exec-path-from-shell-initialize)
-  (exec-path-from-shell-copy-envs
-   '("PATH")))
-
-;; These customizations make it easier for you to navigate files,
-;; switch buffers, and choose options from the minibuffer.
-(load "navigation.el")
-
-;; These customizations change the way emacs looks and disable/enable
-;; some user interface elements
-(load "ui.el")
-
-;; These customizations make editing a bit nicer.
-(load "editing.el")
-
-;; For editing lisps
-(load "elisp-editing.el")
-
-;; Langauage-specific
-(load "setup-clojure.el")
-(load "setup-js.el")
-(load "org-mode.el")
-
-(if (display-graphic-p)
-    (load "guistuff.el"))
-
-;;;;;;;;; IJS Specific ;;;;;;;;;;;;;;
-
-;; Separate stuff that is for graphics mode only
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
-;; Set remote user to root by default
-(setq tramp-default-user "root")
+;; log starting time to come up with a duration afterwards
+(defvar my-init-el-start-time (current-time) "Time when init.el was started")
 
-;; Highlight whole expression on paren match, not just other bracket
-(setq show-paren-style 'expression)
+(setq my-user-emacs-directory "~/.emacs.d/")
 
-(setq cider-repl-use-pretty-printing t)
+;; set paths to manually installed Org-mode (from git; instead of built-in Org-mode)
+(add-to-list 'load-path (concat my-user-emacs-directory "contrib/org-mode/lisp"))
+(add-to-list 'load-path (concat my-user-emacs-directory "contrib/org-mode/contrib/lisp"))
 
-;; Nicer font on OSX
-(set-face-attribute 'default nil :family "Source Code Pro" :weight 'Light  :height 160)
+(require 'org)
 
-;; completion help 
-(which-key-mode)
+;; =======================================================================================
+;; The init.el file looks for "config.org" and tangles its elisp blocks (matching
+;; the criteria described below) to "config.el" which is loaded as Emacs configuration.
+;; Inspired and copied from: http://www.holgerschurig.de/en/emacs-init-tangle/
+;; =======================================================================================
 
-;; Sane mouse scroll wheel
-(setq mouse-wheel-scroll-amount '(3))
-(setq mouse-wheel-progressive-speed nil)
 
-(setq-default cursor-type 'box)
+;; from: http://stackoverflow.com/questions/251908/how-can-i-insert-current-date-and-time-into-a-file-using-emacs
+(defvar current-date-time-format "%a %b %d %Y-%m-%dT%H:%M:%S "
+  "Format of date to insert with `insert-current-date-time' func
+See help of `format-time-string' for possible replacements")
 
-;; Nice Completion mode
-(global-set-key (kbd "TAB") #'company-indent-or-complete-common)
-(global-company-mode)
+;; from: http://stackoverflow.com/questions/251908/how-can-i-insert-current-date-and-time-into-a-file-using-emacs
+(defvar current-time-format "%a %H:%M:%S"
+  "Format of date to insert with `insert-current-time' func.
+Note the weekly scope of the command's precision.")
 
-;; Wavering on auto save. Might trash a conf file if done on command line.
-;; Even GUI mode migt kill a remote file when using Tramp.
-;;(setq auto-save-visited-file-name nil)
-;;(setq auto-save-timeout 5)
 
-;; Mmmmmm hoopy symbols like λ
-(global-prettify-symbols-mode +1)
+(defun my-tangle-config-org ()
+  "This function will write all source blocks from =config.org= into =config.el= that are ...
+
+- not marked as =tangle: no=
+- doesn't have the TODO state =DISABLED=
+- have a source-code of =emacs-lisp="
+  (require 'org)
+  (let* ((body-list ())
+         (output-file (concat my-user-emacs-directory "config.el"))
+         (org-babel-default-header-args (org-babel-merge-params org-babel-default-header-args
+                                                                (list (cons :tangle output-file)))))
+    (message "—————• Re-generating %s …" output-file)
+    (save-restriction
+      (save-excursion
+        (org-babel-map-src-blocks (concat my-user-emacs-directory "config.org")
+	  (let* (
+		 (org_block_info (org-babel-get-src-block-info 'light))
+		 ;;(block_name (nth 4 org_block_info))
+		 (tfile (cdr (assq :tangle (nth 2 org_block_info))))
+		 (match_for_TODO_keyword)
+		 )
+	    (save-excursion
+	      (catch 'exit
+		;;(when (string= "" block_name)
+		;;  (message "Going to write block name: " block_name)
+		;;  (add-to-list 'body-list (concat "message(\"" block_name "\")"));; adding a debug statement for named blocks
+		;;  )
+		(org-back-to-heading t)
+		(when (looking-at org-outline-regexp)
+		  (goto-char (1- (match-end 0))))
+		(when (looking-at (concat " +" org-todo-regexp "\\( +\\|[ \t]*$\\)"))
+		  (setq match_for_TODO_keyword (match-string 1)))))
+	    (unless (or (string= "no" tfile)
+			(string= "DISABLED" match_for_TODO_keyword)
+			(not (string= "emacs-lisp" lang)))
+	      (add-to-list 'body-list (concat "\n\n;; #####################################################################################\n"
+					      "(message \"config • " (org-get-heading) " …\")\n\n")
+			   )
+	      (add-to-list 'body-list body)
+	      ))))
+      (with-temp-file output-file
+	;; Thanks for http://irreal.org/blog/?p=6236 and https://github.com/marcowahl/.emacs.d/blob/master/init.org for the read-only-trick:
+        (insert ";; config.el --- This is the GNU/Emacs config file of Karl Voit. -*- eval: (read-only-mode 1) -*-\n")
+        (insert ";; ======================================================================================\n")
+        (insert ";; Don't edit this file, edit config.org' instead ...\n")
+        (insert ";; Auto-generated at " (format-time-string current-date-time-format (current-time)) "on host " system-name "\n")
+        (insert ";; ======================================================================================\n\n")
+        (insert (apply 'concat (reverse body-list))))
+      (message "—————• Wrote %s" output-file))))
+
+
+;; following lines are executed only when my-tangle-config-org-hook-func()
+;; was not invoked when saving config.org which is the normal case:
+(let ((orgfile (concat my-user-emacs-directory "config.org"))
+      (elfile (concat my-user-emacs-directory "config.el"))
+      (gc-cons-threshold most-positive-fixnum))
+  (when (or (not (file-exists-p elfile))
+            (file-newer-than-file-p orgfile elfile))
+    (my-tangle-config-org)
+    ;;(save-buffers-kill-emacs);; TEST: kill Emacs when config has been re-generated due to many issues when loading newly generated config.el
+    )
+  (load-file elfile))
+
+;; when config.org is saved, re-generate config.el:
+(defun my-tangle-config-org-hook-func ()
+  (when (string= "config.org" (buffer-name))
+	(let ((orgfile (concat my-user-emacs-directory "config.org"))
+		  (elfile (concat my-user-emacs-directory "config.el")))
+	  (my-tangle-config-org))))
+(add-hook 'after-save-hook 'my-tangle-config-org-hook-func)
+
+
+(message "→★ loading init.el in %.2fs" (float-time (time-subtract (current-time) my-init-el-start-time)))
+
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-contacts-address-property "CITY" t)
+ '(org-contacts-birthday-property "BORN" t)
+ '(org-contacts-files "~/share/all/org-mode/contacts.org" t)
+ '(org-contacts-icon-property "PHOTOGRAPH" t)
+ '(safe-local-variable-values
+   (quote
+    ((eval ispell-change-dictionary "en_US")
+     (TeX-master . "misc")
+     (TeX-master . "references")
+     (eval org-expiry-deinsinuate)))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
